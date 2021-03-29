1dcc477c1ca637484454ad8d2f9a9ea7e3dcc3b7
Romindo <80807400+romindo1@users.noreply.github.com>
2021-03-29 10:54:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!-- Template by freewebsitetemplates.com -->
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Oentong</title>
+<link rel="stylesheet" type="text/css" href="style1.css" media="all" />
+</head>
+<body>
+	<div id="header">
+			<div id="logo">
+				<a href="index.php"><img height="170" width="380" src="images/logo2.gif" alt="" /></a>		
+			</div>		
+			<ul>
+				<li class="selected"><a href="index.php"><span>Home</span></a></li>
+				<li><a href="about.php"><span>About Us</span></a></li>
+				<li><a href="produk/products.php"><span>Products</span></a></li>
+				<li><a href="contact.php"><span>Contact Us</span></a></li>			
+			</ul>
+	</div>
+<div id="body1">
+			
+<br><br><br><br><br><br>			
+<table border="0" align="center" width="90%" bgcolor="#EFFBFB" cellpadding="2" cellspacing="10">
+ <tr>
+ <td><font face ="arial" size="5" color="grey">
+<center>Selamat datang di website oentong<br></font>
+<font face ="arial" size="4" color="grey">
+Di dalam website ini Anda dapat memesan Helm yang<br>Anda ingin sesuai dengan ukuran Anda dan harga yang terjangkau.</center>
+</font>
+<br><br><font color="grey">
+<b>www.oentong.com</b> bertujuan untuk memperkenalkan produk helm motor dengan model dan desain yang terbaik.
+<p align="justify"><font face ="arial">
+Di era digital ini kami berkomitmen untuk memberikan pelayanan serta kemudahan bagi anda para pelanggan untuk memesan produk kami secara online. Kami juga tidak menutup kemungkinan untuk melayani pembelian anda secara COD (Cash On Delivery) *khusus untuk daerah tertentu sesuai dengan perjanjian, memperkenalkan outlet untuk mengetahui dimana saja anda bisa mendapatkan helm yang Anda inginkan, dan menerima masukan dan saran dari anda.
+
+<P align="justify">Helm yang tersedia oleh www.oentong.com sendiri merupakan produk yang sudah teruji keamanannya. Kami telah mendapatkan SNI (Standar Nasional Indonesia) dari BSN (Badan Standardisasi Nasional).
+
+Dapatkan juga berbagai informasi produk terbaru, produk spesial, produk yang sedang diskon dan voucher diskon dari www.oentong.com langsung ke halaman contact yag telah disediakan.
+ 
+ </td>
+
+ <td align="left" bgcolor="#EFFBFB"><?php include"slideshow.php"?></td>
+ </tr>
+</table>
+</div>					
+<br><br><br>
+	<div id="footer">
+			<div>
+				<div>
+					<h3></h3>
+					<ul>
+						<li></li>				
+						<li></li>
+					</ul>			
+				</div>		
+				<div>
+					<h3></h3>
+					<ul>
+						<li></li>				
+						<li></li>
+					</ul>			
+				</div>	
+				<div>
+					<h3>follow us:</h3>
+					<a class="facebook" href="http://facebook.com/freewebsitetemplates" target="_blank">facebook</a>		
+					<a class="twitter" href="http://twitter.com/fwtemplates" target="_blank">twitter</a>
+				</div>	
+				<div>
+					<h3></h3>
+					<ul>
+						<li></li>				
+						<li></li>
+					</ul>			
+				</div>	
+				<div>
+				</div>	
+			</div>
+			<div>
+				<p>&copy Copyright 2017. Dian Rizky</p>
+			</div>
+		</div>
+	</body>
+</html>
